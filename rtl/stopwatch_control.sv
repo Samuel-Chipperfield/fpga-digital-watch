1535c8940b97425033983cef1361941c871f7872
Samuel-Chipperfield <samuel.chipperfield@student.unimelb.edu.au>
2026-05-05 22:23:57 +1000

    Update stopwatch_control.sv

--- rtl/stopwatch_control.sv
+++ rtl/stopwatch_control.sv
@@ -1,22 +1,40 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
 module stopwatch_control (
-    input logic clk,
-    input logic rise_start_stop,
-    input logic rise_lap,
+    input  logic clk,
+    input  logic rise_start_stop,
+    input  logic rise_lap,
     output logic counter_rst,
     output logic counter_enable,
     output logic lap_hold
 );
-logic [2:0] state = {counter_rst,counter_enable,lap_hold};
-initial state = 3'd0;
-logic rise_start;
-always_ff@(posedge clk) rise_start<=rise_start_stop;
-assign counter_enable = (rise_start == 1)?  !counter_enable:counter_enable;
-assign lap_hold = (counter_enable && rise_lap)? !lap_hold:lap_hold;
-always_comb begin
-    case(state) begin
-        3'b000:
-    endcase
-end
+
+  logic [2:0] state;
+  initial counter_enable = 1'b0;
+  initial counter_rst = 1'b0;
+  initial lap_hold = 1'b0;
+  logic [2:0] next_state;
+  assign state = {counter_rst, counter_enable, lap_hold};
+  logic ss_only = rise_start_stop && !rise_lap;
+  logic lap_only = !rise_start_stop && rise_lap;
+  assign next_state[1] = (ss_only == 1) ? !state[1] : state[1];
+  assign next_state[2] = (lap_only && !state[1] && !state[0]) ? 1'b1 : 1'b0;
+
+  always_comb begin
+    if (lap_only) begin
+      case (state)
+        3'b010: next_state[0] = 1;
+        3'b011: next_state[0] = 0;
+        default next_state[0] = 0;
+      endcase
+    end else next_state[0] = state[0];
+  end
+  always_ff @(posedge clk) begin
+    counter_rst <= next_state[2];
+    counter_enable <= next_state[1];
+    lap_hold <= next_state[0];
+  end
+
 endmodule
+
+
